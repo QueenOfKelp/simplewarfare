3e23b717bc9ea9c5591143fc59d83b4b3e6d0255
Melody <funnyshackinc@gmail.com>
2023-06-28 14:48:17 -0600

    modified some code so that the gun tooltips show more accurate information and guns can now get attachments put on them

--- src/main/java/queenofkelp/simplewarfare/util/damage_dropoff/DamageDropoff.java
+++ src/main/java/queenofkelp/simplewarfare/util/damage_dropoff/DamageDropoff.java
@@ -5,4 +5,8 @@ public abstract class DamageDropoff {
     public float getDamageForDistance(double distance, float damage) {
         return 1;
     }
+
+    public String getDisplayInformation() {
+        return "no info to display";
+    }
 }
