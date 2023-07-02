c6251b446ce840712d54d5bb946fc89ff0273fcd
Melody <funnyshackinc@gmail.com>
2023-07-02 14:18:42 -0600

    hopefully fix

--- src/main/java/queenofkelp/simplewarfare/bullet/item/AmmoType.java
+++ src/main/java/queenofkelp/simplewarfare/bullet/item/AmmoType.java
@@ -2,7 +2,7 @@ package queenofkelp.simplewarfare.bullet.item;
 
 import net.minecraft.text.Text;
 
-public enum AmmoType {
+public enum AmmoType implements IAmmoType{
     MEDIUM(Text.literal("5.56")),
     HEAVY(Text.literal(".50"));
 
---
+++ src/main/java/queenofkelp/simplewarfare/bullet/item/IAmmoType.java
@@ -0,0 +1,7 @@
+package queenofkelp.simplewarfare.bullet.item;
+
+import net.minecraft.text.Text;
+
+public interface IAmmoType {
+    public final Text displayName = null;
+}
